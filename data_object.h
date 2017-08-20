bc15584d2fcf67311c85bfeeaa07e730b5b787dc
Moritz Ulmer <moritz.ulmer@posteo.de>
2017-08-20 12:41:05 +0200

    Cleanup code formatting

--- data_object.h
+++ data_object.h
@@ -7,57 +7,53 @@
 using namespace std;
 
 template <typename T>
-size_t size(const T& x)
-{ return sizeof x; }
+size_t size(const T& x) {
+  return sizeof x;
+}
 
 template <typename T>
-size_t serialize(const T& x, uint8_t* buffer, size_t bufferSize)
-{
-    size_t bytesToWrite = size(x);
-    if(bytesToWrite <= bufferSize)
-    {
-        memcpy(buffer, &x, bytesToWrite);
+size_t serialize(const T& x, uint8_t* buffer, size_t bufferSize) {
+  size_t bytesToWrite = size(x);
+  if(bytesToWrite <= bufferSize) {
+      memcpy(buffer, &x, bytesToWrite);
+    } else {
+      bytesToWrite = 0;
     }
-    else
-    {
-        bytesToWrite = 0;
-    }
-    return bytesToWrite;
+  return bytesToWrite;
 }
 
 class Object {
-  public:
-    template <typename T>
-    Object(T x) : self_(new model<T>(move(x)))
-    { }
-    
-    friend size_t serialize(const Object& x, uint8_t* buffer, size_t bufferSize)
-    { x.self_->serialize_(buffer, bufferSize); }
-    friend size_t size(const Object& x)
-    { x.self_->size_(); }
-    
-    private:
-      struct Concept {
-          virtual ~Concept() = default;
-          virtual size_t serialize_(uint8_t*, size_t bufferSize) const = 0;
-          virtual size_t size_() const = 0;
-      };
-      template <typename T>
-      struct model : Concept {
-          model(T x) : data_(move(x)) { }
-          size_t serialize_(uint8_t* buffer, size_t bufferSize) const
-          { serialize(data_, buffer, bufferSize); }
-          size_t size_() const
-          { size(data_); }
-          
-          T data_;
+public:
+  template <typename T>
+  Object(T x) : self_(new model<T>(move(x)))
+  { }
 
-      };
-      
-      shared_ptr<const Concept> self_;
+  friend size_t serialize(const Object& x, uint8_t* buffer, size_t bufferSize) {
+    x.self_->serialize_(buffer, bufferSize);
+  }
+  friend size_t size(const Object& x) {
+    x.self_->size_();
+  }
+
+private:
+  struct Concept {
+    virtual ~Concept() = default;
+    virtual size_t serialize_(uint8_t*, size_t bufferSize) const = 0;
+    virtual size_t size_() const = 0;
+  };
+  template <typename T>
+  struct model : Concept {
+    model(T x) : data_(move(x)) { }
+    size_t serialize_(uint8_t* buffer, size_t bufferSize) const
+    { serialize(data_, buffer, bufferSize); }
+    size_t size_() const
+    { size(data_); }
+
+    T data_;
+
+  };
+
+  shared_ptr<const Concept> self_;
 };
 
 #endif
-
-// TODO: replace uint8_t* with std::array
-
--- main.cpp
+++ main.cpp
@@ -10,82 +10,79 @@
 using namespace std;
 
 class MyClass {
-  public:
-    MyClass(const string& name) :
-        name_(name)
-    { }
+public:
+  MyClass(const string& name) :
+    name_(name)
+  { }
 
-    friend size_t serialize(const MyClass& x, uint8_t* buffer, size_t bufferSize);
-    friend size_t size(const MyClass& x);
-  private:
-    const string name_;
+  friend size_t serialize(const MyClass& x, uint8_t* buffer, size_t bufferSize);
+  friend size_t size(const MyClass& x);
+private:
+  const string name_;
 };
 
-size_t serialize(const MyClass& x, uint8_t* buffer, size_t bufferSize)
-{
-    size_t bytesToWrite = size(x);
-    if(bytesToWrite <= bufferSize){
-        memcpy(buffer, x.name_.c_str(), bytesToWrite);
+size_t serialize(const MyClass& x, uint8_t* buffer, size_t bufferSize) {
+  size_t bytesToWrite = size(x);
+  if(bytesToWrite <= bufferSize) {
+      memcpy(buffer, x.name_.c_str(), bytesToWrite);
     } else {
-        bytesToWrite = 0;
+      bytesToWrite = 0;
     }
-    return bytesToWrite;
+  return bytesToWrite;
 }
 
-size_t size(const MyClass& x)
-{ x.name_.length(); }
+size_t size(const MyClass& x) {
+  x.name_.length();
+}
 
 /******************************************************************************/
 
 using Channel = vector<Object>;
 
-size_t size(const Channel& x)
-{
-    size_t totalSize = 0;
-    for (auto& e : x) totalSize += size(e);
-    return totalSize;
+size_t size(const Channel& x) {
+  size_t totalSize = 0;
+  for (auto& e : x) {
+      totalSize += size(e);
+    }
+  return totalSize;
 }
 
-size_t serialize(const Channel& x, uint8_t* buffer, size_t bufferSize)
-{
-    size_t bytesToWrite = size(x);
-    size_t offset = 0;
-    if(bytesToWrite <= bufferSize)
-    {
-        for (auto& e : x)
-        {
-            offset += serialize(e, buffer + offset, bufferSize - offset);
+size_t serialize(const Channel& x, uint8_t* buffer, size_t bufferSize) {
+  size_t bytesToWrite = size(x);
+  size_t offset = 0;
+  if(bytesToWrite <= bufferSize) {
+      for (auto& e : x) {
+          offset += serialize(e, buffer + offset, bufferSize - offset);
         }
     } else {
-        bytesToWrite = 0;
+      bytesToWrite = 0;
     }
-    return bytesToWrite;
+  return bytesToWrite;
 }
 
 /******************************************************************************/
 
-int main()
-{
-    Channel channel;
-    uint8_t buffer[20] = {};
+int main() {
+  Channel channel;
+  uint8_t buffer[20] = {};
 
-    channel.emplace_back(MyClass(string("hi")));
-    channel.emplace_back(' ');
-    channel.emplace_back(MyClass(string("there")));
-    serialize(channel, buffer, sizeof buffer);
-    cout << buffer << endl;
-    
-    cout << "-------------------" << endl;
+  channel.emplace_back(MyClass(string("hi")));
+  channel.emplace_back(' ');
+  channel.emplace_back(MyClass(string("there")));
+  serialize(channel, buffer, sizeof buffer);
+  cout << buffer << endl;
 
-    cout << size(channel) << endl;
+  cout << "-------------------" << endl;
 
-    cout << "-------------------" << endl;
+  cout << size(channel) << endl;
 
-    Channel channel2;
-    channel2.emplace_back(channel);
-    channel2.emplace_back(channel);
-    serialize(channel2, buffer, sizeof buffer);
-    cout << buffer << endl;
+  cout << "-------------------" << endl;
+
+  Channel channel2;
+  channel2.emplace_back(channel);
+  channel2.emplace_back(channel);
+  serialize(channel2, buffer, sizeof buffer);
+  cout << buffer << endl;
 }
 
 #endif
